63cac4c8015034acda901b294bb4cc1f524a23d8
Joshua Bissell <joahgreat12@gmail.com>
2025-10-04 20:02:13 +0100

    Pause menu in Megajam scene

--- Assets/Scripts/UI/PauseMenuController.cs
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class PauseMenuController : BookMenuController
 {
+	public UnityEvent onResume = new();
+
 	/// <summary>
 	/// Resume the game
 	/// </summary>
 	public void ResumeButton()
 	{
-
+		onResume.Invoke();
 	}
 
 	/// <summary>
@@ -21,5 +25,10 @@ public class PauseMenuController : BookMenuController
 	public void QuitButton()
 	{
 		SceneController.Instance.StartSceneSwitch(SceneType.MainMenu);
+	}
+
+	public void SetBookUpState(bool value)
+	{
+		bookAnimator.SetBool("book_up", value);
 	}
 }
