8bc3d79e361c06410a318b8c2bee7046152839f5
Joshua Bissell <joahgreat12@gmail.com>
2025-10-05 16:45:11 +0100

    Wrote DialogueEvent

---
+++ Assets/Scripts/Events/DialogueEvent.cs
@@ -0,0 +1,36 @@
+﻿using PixelCrushers.DialogueSystem;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueEvent : Event
+{
+	public override UnityEvent OnEventStart { get; set; }
+	public override UnityEvent OnEventEnd { get; set; }
+
+	[SerializeField]
+	private DialogueSystemEvents _dialogueSystemEvents;
+
+	[SerializeField]
+	private string _conversationName;
+
+	private UnityAction<Transform> endConversationAction;
+
+	public override void StartEvent()
+	{
+		endConversationAction = (transform) => EndEvent();
+		_dialogueSystemEvents.conversationEvents.onConversationEnd.AddListener(endConversationAction);
+		DialogueManager.StartConversation(_conversationName);
+		OnEventStart?.Invoke();
+		Debug.Log($"Started dialogue event with conversation {_conversationName}");
+	}
+
+	public override void EndEvent()
+	{
+		_dialogueSystemEvents.conversationEvents.onConversationEnd.RemoveListener(endConversationAction);
+		OnEventEnd?.Invoke();
+		Debug.Log("Ended dialogue event");
+	}
+
+	public override string GetType() => "Dialogue Event";
+}
