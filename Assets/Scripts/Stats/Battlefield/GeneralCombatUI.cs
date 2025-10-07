76397ddea746510a80b86c0d9034ee705b6d6ee3
Darius Fang <dariusfang1@gmail.com>
2025-10-07 15:09:12 -0600

    updated combat with more text

--- Assets/Scripts/Stats/Battlefield/GeneralCombatUI.cs
+++ Assets/Scripts/Stats/Battlefield/GeneralCombatUI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class GeneralCombatUI : MonoBehaviour
 {
@@ -8,11 +9,14 @@ public class GeneralCombatUI : MonoBehaviour
     public TextMeshProUGUI DefendTextAction;
     public TextMeshProUGUI HealTextAction;
     public TextMeshProUGUI ManaTextAction;
+    String willDrainText = "\n+10 Will";
+    String useWillText = "\n-10 Will";
 
-    public void UpdateButtonText(UpgradedAbilities upgradedAbilities) {
-        AttackTextAction.text = upgradedAbilities.AttackUpgraded ? "Attack (Upgraded)" : "Attack";
-        DefendTextAction.text = upgradedAbilities.DefenseUpgraded ? "Defend (Upgraded)" : "Defend";
-        HealTextAction.text = upgradedAbilities.HealUpgraded ? "Heal (Upgraded)" : "Heal";
-        ManaTextAction.text = upgradedAbilities.ManaUpgraded ? "Will (Upgraded)" : "Will";
+    public void UpdateButtonText(UpgradedAbilities upgradedAbilities)
+    {
+        AttackTextAction.text = upgradedAbilities.AttackUpgraded ? "Attack+" + useWillText : "Attack" + useWillText;
+        DefendTextAction.text = upgradedAbilities.DefenseUpgraded ? "Defend+" + useWillText : "Defend" + useWillText;
+        HealTextAction.text = upgradedAbilities.HealUpgraded ? "Heal+" + useWillText: "Heal" + useWillText;
+        ManaTextAction.text = upgradedAbilities.ManaUpgraded ? "Will Drain+\n+15 Will" : "Will Drain" + willDrainText;
     }
 }
